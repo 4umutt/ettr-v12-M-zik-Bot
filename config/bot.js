59c94a364bf416b8579e09dc185c0353fec28443
4umutt <109687244+4umutt@users.noreply.github.com>
2022-07-20 20:05:35 +0300

    Initial commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'OTk5MzQzOTI2MjE1NDQ2NTY4.GIxjPp.1jZvPx_Joa91uuwexj7gK3QeAXJCzk3jz__Fc8', // Botun Tokeni
+        prefix: '+', // Botun Prefixi
+        activity: 'müzik|+play', // Botun Oynuyor Kısmı
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
